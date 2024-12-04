dd0bd611fa8fbfc219b69e08e195185226ce7a25
Alishan Shaikh <rwbn1.alishan.as@gmail.com>
2024-12-04 09:46:54 +0530

    all c programs first

---
+++ C Languages/1_C/index.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main(){
+		
+//		printf("hello, vaishali how's you college ? \n");
+//		printf("hi, pratik, how you doing ?");
+
+	
+	printf("* * * * *           *                     \n");
+	printf("*                  * *                         \n");
+	printf("*                 *   *                          \n");
+	printf("*                *     *                     \n");
+	printf("* * * *         * * * * *                       \n");
+	printf("*              *         *                     \n");
+	printf("*             *           *               \n");
+	printf("*            *             *                 \n");
+	printf("* * * * *   *               *                \n");
+	
+}
---
+++ C Languages/3_C_scanf/swap.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+	
+	int a = 13;  
+	int b = 19;	
+	int temp;    
+
+	temp = a;   // temp = 13;
+	a = b;      // a = b = 19;
+	b = temp;   // b = temp = 13;
+	
+	printf("a: %d \n", a);
+	printf("b: %d", b);
+}
